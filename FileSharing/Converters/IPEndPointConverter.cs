4bb653daf6905940c9af6c0078ee9b835cc08070
Dexp <Dexp@DESKTOP-G7BK324>
2022-01-19 14:36:06 +0900

    Добавьте файлы проекта.

---
+++ FileSharing/Converters/IPEndPointConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Globalization;
+using System.Windows.Data;
+using System.Net;
+
+namespace FileSharing.Converters
+{
+    public class IPEndPointConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as IPEndPoint).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ FileSharing/InputBox/InputBox.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+
+namespace FileSharing.InputBox
+{
+    public partial class InputBox : Window
+    {
+        public InputBox(string title, string question, string defaultAnswer = "")
+        {
+            InitializeComponent();
+
+            Title = title;
+            lblQuestion.Content = question;
+            txtAnswer.Text = defaultAnswer;
+        }
+
+        public string Answer => txtAnswer.Text;
+
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            txtAnswer.SelectAll();
+            txtAnswer.Focus();
+        }
+    }
+}
